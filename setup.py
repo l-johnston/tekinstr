8d32b424d845f6630802c1342b261d5d3748c0b3
Lee Johnston <lee.johnston@ni.com>
2020-01-13 02:00:06 -0600

    BLD: update setup.py and pipeline

--- setup.py
+++ setup.py
@@ -1,13 +1,40 @@
 """Setuptools install script"""
-from setuptools import setup
+from setuptools import setup, find_packages
+
+
+def get_readme():
+    """Get readme contents"""
+    with open("README.md") as f:
+        return f.read()
+
+
+def get_version():
+    """Get the version number"""
+    with open("tekinstr/version.py") as f:
+        lines = f.readlines()
+    line = ""
+    for line in lines:
+        if line.startswith("__version__"):
+            break
+    version = [s.strip().strip('"') for s in line.split("=")][1]
+    return version
+
 
 setup(
     name="tekinstr",
-    version="0.0.0",
+    version=get_version(),
     author="Lee Johnston",
     author_email="lee.johnston.100@gmail.com",
     description="Communication with Tektronix oscilloscopes",
-    packages=["tekinstr"],
+    long_description=get_readme(),
+    packages=find_packages(),
     license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Science/Research",
+    ],
+    url="https://github.com/l-johnston/tekinstr",
     install_requires=["numpy", "sympy", "unit_system", "pyvisa"],
 )
